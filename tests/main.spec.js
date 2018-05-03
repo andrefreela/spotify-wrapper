27ab8d980cc501983513479850907e611a8431e6
André Francisco <eu@andrefrancisco.com.br>
2018-05-03 11:17:09 -0300

    TypeError: Attempted to wrap fetch which is already wrapped

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,72 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+chai.use(sinonChai);
+sinonStubPromise(sinon);
 
-describe('Main', () => {
+global.fetch = require('node-fetch');
 
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
+
+describe('Spotify Wrapper', () => {
+
+  describe('smoke tests', () => {
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+
+  describe('Generic Search', () => {
+    let fetchedStub;
+
+    beforeEach( () => {
+      fetchedStub = sinon.stub(global, 'fetch');
+      fetchedStub.returnsPromise();
+    });
+
+    afterEach( () => {
+      fetchedStub.restore();
+    });
+
+    it('should call fetch function', () => {
+      const artists = search();
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    context('passing one type', () => {
+      it('should receive the correct URL', () => {
+        const artists = search('incubus', 'artist');
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=incubus&type=artist');
+
+        const albums = search('incubus', 'album');
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=incubus&type=album');
+      });
+    });
+
+    context('passing more than one type', () => {
+      const artistsAndAlbums = search('incubus', ['artist', 'album']);
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=incubus&type=artist,album');
+    });
+
+  });
 });
